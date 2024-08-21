e5dad3012ded87bddecdeaa77a0d3f1724b3f617
Ayush Pandey <ayush12122003@gmail.com>
2024-08-22 00:55:02 +0530

    youtube_videodownload

--- Django/youtubeDownloader/youtubeDownloader/urls.py
+++ Django/youtubeDownloader/youtubeDownloader/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
+from youtube_site import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('weather_site.urls')),
-   
+    path('', include('youtube_site.urls')),
+    
 ]
